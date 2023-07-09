4e05a19d4bd55a9857b3f94e047caac554022d4f
Maxwell Reid <maxwell.reid@gmail.com>
2023-07-09 18:53:25 +1000

    Add sorting and traversing task days/months

--- frontend/src/pages/Calendar/Calendar.tsx
+++ frontend/src/pages/Calendar/Calendar.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from 'react'
+import { useEffect, useMemo, useState } from 'react'
+import dayjs, { Dayjs } from 'dayjs'
 import { ChevronFirst, ChevronLast, ChevronLeft, ChevronRight } from 'lucide-react'
 import { useLocation } from 'wouter'
 
 import { DayGlobe, Header, Spinner } from '/src/components'
+import { Calendar } from '/src/config/api'
 import { useCalendarStore } from '/src/hooks'
 import { useAuthStore } from '/src/hooks/useAuth'
 
@@ -12,52 +14,78 @@ const Main = () => {
   const { isAuthLoading, session, signOut } = useAuthStore()
   const { calendar, loading: isCalendarLoading, getCalendar } = useCalendarStore()
   const [_, setLocation] = useLocation()
-  console.log(calendar)
+
   useEffect(() => {
     if (!isAuthLoading) {
       !session ? setLocation('/login') : getCalendar()
     }
   }, [isAuthLoading, session])
 
+  const [currentMonth, setCurrentMonth] = useState<Dayjs>(dayjs().startOf('month'))
+  const [firstMonth, setFirstMonth] = useState<Dayjs | undefined>()
+  const [lastMonth, setLastMonth] = useState<Dayjs | undefined>()
+
+  const sortedCalendar = useMemo(() => calendar &&
+    Object.keys(calendar).sort((a, b) => dayjs(a).isBefore(dayjs(b)) ? -1 : 1)
+      .reduce((obj, key) => ({...obj, [key]: calendar[key]}), {})
+  , [calendar])
+
+  useEffect(() => {
+    if (sortedCalendar) {
+      const sortedDates = Object.keys(sortedCalendar)
+      setFirstMonth(dayjs(sortedDates.at(0)).startOf('month'))
+      setLastMonth(dayjs(sortedDates.at(-1)).startOf('month'))
+    }
+  }, [sortedCalendar])
+
+  const currentMonthTasks: Calendar | null = useMemo(() => calendar &&
+    Object.keys(calendar)
+      .filter(date => dayjs(date).month() === currentMonth.month())
+      .sort((a, b) => dayjs(a).isBefore(dayjs(b)) ? -1 : 1)
+      .reduce((obj, key) => ({...obj, [key]: calendar[key]}), {})
+  , [calendar, currentMonth])
 
   const handleSignout = () => {
     signOut()
   }
 
+  const [openedDate, setOpenedDate] = useState<string | undefined>(undefined)
+  const openedDayTasks = useMemo(() => (openedDate && currentMonthTasks) ? currentMonthTasks[openedDate].sort((a, b) => a.order - b.order) : [], [currentMonthTasks, openedDate])
+
   return (<>
     <Header />
     <ControlsContainer>
       <ControlButton
-        disabled={false}
-        onClick={(() => console.log('End left'))}>
+        disabled={firstMonth?.isSame(currentMonth)}
+        onClick={(() => setCurrentMonth(firstMonth || currentMonth))}>
         <ChevronFirst />
       </ControlButton>
 
       <ControlButton
-        disabled={false}
-        onClick={() => console.log('Left')}>
+        disabled={firstMonth?.isSame(currentMonth)}
+        onClick={() => setCurrentMonth(currentMonth.subtract(1, 'month'))}>
         <ChevronLeft />
       </ControlButton>
 
-      <ControlMonth>DECEMBER</ControlMonth>
+      <ControlMonth>{currentMonth.format('MMMM')}</ControlMonth>
 
       <ControlButton
-        disabled={false}
-        onClick={() => console.log('Right')}>
+        disabled={lastMonth?.isSame(currentMonth)}
+        onClick={() => setCurrentMonth(currentMonth.add(1, 'month'))}>
         <ChevronRight />
       </ControlButton>
 
       <ControlButton
-        disabled={true}
-        onClick={(() => console.log('End Right'))}>
+        disabled={lastMonth?.isSame(currentMonth)}
+        onClick={(() => setCurrentMonth(lastMonth || currentMonth))}>
         <ChevronLast />
       </ControlButton>
     </ControlsContainer>
 
     <MonthContainer>
-      {isCalendarLoading || !calendar ?
+      {isCalendarLoading || !currentMonthTasks ?
         <Spinner center={true} /> : <>
-          {Object.entries(calendar).forEach(([date, tasks]) => (<DayGlobe key={date} />))}
+          {Object.entries(currentMonthTasks).map(([date, tasks]) => (<DayGlobe key={date} date={date} />))}
         </>
       }
     </MonthContainer>

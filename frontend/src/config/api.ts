3b4ed34a01f04f08d8b5856ffaee6678ec8cb7fa
Maxwell Reid <maxwell.reid@gmail.com>
2023-07-09 20:39:59 +1000

    Implement Patch method on frontend

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -14,6 +14,19 @@ const get = async <S extends z.Schema>(url: string, schema: S, access_token: str
   return schema.parse(await res.json())
 }
 
+const patch = async <S extends z.Schema>(url: string, schema: S, access_token: string): Promise<ReturnType<S['parse']>> => {
+  const res = await fetch(new URL(url, env.API_URL), {
+    method: 'PATCH',
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  })
+    .catch(console.warn)
+
+  if (!res?.ok) throw res
+  return schema.parse(await res.json())
+}
+
 export const Task = z.object({
   id: z.string(),
   user_id: z.string(),

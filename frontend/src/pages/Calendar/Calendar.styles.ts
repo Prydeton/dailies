3c6a420d8b140a27d4abeb7f85ce08afceba6bfd
Maxwell Reid <maxwell.reid@gmail.com>
2023-07-16 10:39:53 +1000

    Add year display to calendar page

--- frontend/src/pages/Calendar/Calendar.styles.ts
+++ frontend/src/pages/Calendar/Calendar.styles.ts
@@ -1,21 +1,23 @@
 import { styled } from 'goober'
 
 export const PageContainer = styled('main')`
-    display: flex;
-    flex-direction: column;
-    position: relative;
+  display: flex;
+  flex-direction: column;
+  position: relative;
 `
 
 export const ControlsWrapper = styled('div')`
   width: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
 `
 
 export const ControlsContainer = styled('div')`
   width: 100%;
   box-sizing: border-box;
-  padding: 10px 20px;
+  padding: 10px 20px 5px 20px;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -42,7 +44,7 @@ export const ControlButton = styled('button')`
 export const ControlMonth = styled('h2')`
   padding: 0;
   margin: 0;
-  min-width: 111px;
+  min-width: 180px;
   display: grid;
   place-content: center;
   letter-spacing: 5px;
@@ -56,4 +58,8 @@ export const MonthContainer = styled('div')`
   padding: 20px;
   max-height: calc(100vh - 95px - 82px);
   overflow-y: scroll;
+`
+
+export const ControlYear = styled('span')`
+  text-align: center;
 `
--- frontend/src/pages/Calendar/Calendar.tsx
+++ frontend/src/pages/Calendar/Calendar.tsx
@@ -8,7 +8,7 @@ import { Calendar } from '/src/config/api'
 import { useCalendarStore } from '/src/hooks'
 import { useAuthStore } from '/src/hooks/useAuth'
 
-import { ControlButton, ControlMonth, ControlsContainer, ControlsWrapper, MonthContainer, PageContainer } from './Calendar.styles'
+import { ControlButton, ControlMonth, ControlsContainer, ControlsWrapper, ControlYear, MonthContainer, PageContainer } from './Calendar.styles'
 import { Day } from '..'
 
 const Main = () => {
@@ -22,9 +22,9 @@ const Main = () => {
     }
   }, [isAuthLoading, session])
 
-  const [currentMonth, setCurrentMonth] = useState<Dayjs>(dayjs().startOf('month'))
-  const [firstMonth, setFirstMonth] = useState<Dayjs | undefined>()
-  const [lastMonth, setLastMonth] = useState<Dayjs | undefined>()
+  const [currentPage, setCurrentPage] = useState<Dayjs>(dayjs().startOf('month'))
+  const [firstPage, setFirstPage] = useState<Dayjs | undefined>()
+  const [lastPage, setLastPage] = useState<Dayjs | undefined>()
 
   const sortedCalendar = useMemo(() => calendar &&
     Object.keys(calendar).sort((a, b) => dayjs(a).isBefore(dayjs(b)) ? -1 : 1)
@@ -34,17 +34,17 @@ const Main = () => {
   useEffect(() => {
     if (sortedCalendar) {
       const sortedDates = Object.keys(sortedCalendar)
-      setFirstMonth(dayjs(sortedDates.at(0)).startOf('month'))
-      setLastMonth(dayjs(sortedDates.at(-1)).startOf('month'))
+      setFirstPage(dayjs(sortedDates.at(0)).startOf('month'))
+      setLastPage(dayjs(sortedDates.at(-1)).startOf('month'))
     }
   }, [sortedCalendar])
 
   const currentMonthTasks: Calendar | null = useMemo(() => calendar &&
     Object.keys(calendar)
-      .filter(date => dayjs(date).month() === currentMonth.month())
+      .filter(date => dayjs(date).month() === currentPage.month())
       .sort((a, b) => dayjs(a).isBefore(dayjs(b)) ? -1 : 1)
       .reduce((obj, key) => ({...obj, [key]: calendar[key]}), {})
-  , [calendar, currentMonth])
+  , [calendar, currentPage])
 
   const [openedDate, setOpenedDate] = useState<string | undefined>(undefined)
 
@@ -55,31 +55,32 @@ const Main = () => {
     <ControlsWrapper>
       <ControlsContainer>
         <ControlButton
-          disabled={firstMonth?.isSame(currentMonth)}
-          onClick={(() => setCurrentMonth(firstMonth || currentMonth))}>
+          disabled={firstPage?.isSame(currentPage)}
+          onClick={(() => setCurrentPage(firstPage || currentPage))}>
           <ChevronFirst />
         </ControlButton>
 
         <ControlButton
-          disabled={firstMonth?.isSame(currentMonth)}
-          onClick={() => setCurrentMonth(currentMonth.subtract(1, 'month'))}>
+          disabled={firstPage?.isSame(currentPage)}
+          onClick={() => setCurrentPage(currentPage.subtract(1, 'month'))}>
           <ChevronLeft />
         </ControlButton>
 
-        <ControlMonth>{currentMonth.format('MMMM')}</ControlMonth>
+        <ControlMonth>{currentPage.format('MMMM')}</ControlMonth>
 
         <ControlButton
-          disabled={lastMonth?.isSame(currentMonth)}
-          onClick={() => setCurrentMonth(currentMonth.add(1, 'month'))}>
+          disabled={lastPage?.isSame(currentPage)}
+          onClick={() => setCurrentPage(currentPage.add(1, 'month'))}>
           <ChevronRight />
         </ControlButton>
 
         <ControlButton
-          disabled={lastMonth?.isSame(currentMonth)}
-          onClick={(() => setCurrentMonth(lastMonth || currentMonth))}>
+          disabled={lastPage?.isSame(currentPage)}
+          onClick={(() => setCurrentPage(lastPage || currentPage))}>
           <ChevronLast />
         </ControlButton>
       </ControlsContainer>
+      <ControlYear>{currentPage.year()}</ControlYear>
     </ControlsWrapper>
 
     <MonthContainer>

1c148c5cd3b6cd148c7b9cc5a1eb2fd080543147
Maxwell Reid <maxwell.reid@gmail.com>
2023-07-18 14:36:42 +1000

    Fix day number not changing colour with new percentage format

--- frontend/src/components/DayGlobe/DayGlobe.tsx
+++ frontend/src/components/DayGlobe/DayGlobe.tsx
@@ -18,7 +18,7 @@ const DayGlobe: React.FC<DayGlobeProps> = ({ tasks, date, setOpenedDate }: DayGl
     <Container>
       <GlobeWrapper onClick={() => setOpenedDate(date)} disabled={tasks.length === 0}>
         <Wave fillPercentage={fillPercentage}/>
-        <Month style={{color: fillPercentage <= 20 ? 'var(--white)' : 'var(--background-dark)'}}>{new Date(date).getDate()}</Month>
+        <Month style={{color: fillPercentage <= .2 ? 'var(--white)' : 'var(--background-dark)'}}>{new Date(date).getDate()}</Month>
       </GlobeWrapper>
     </Container>
   )
